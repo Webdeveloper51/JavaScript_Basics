e551cee10de683c3bbbf68a694804351a2406679
Webdeveloper51 <53963284+Webdeveloper51@users.noreply.github.com>
2024-07-19 23:43:53 +0100

    datatypes sample

---
+++ learn/datatypes.js
@@ -0,0 +1,9 @@
+let name="USman" //stirng
+let age=30  //number
+let isLogin=false   //boolena
+
+/*
+Undefined => NO Value
+Null => Stand alone value like empty (datatype=>object)
+symbol => unique
+*/
